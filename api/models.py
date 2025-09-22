5b7644d2e147e4ff3c8eb6b2ac208798d4e773da
Sifen-tesfaye <sifen.tesfaye@aastustudent.edu.et>
2025-09-22 00:28:14 -0700

    Add Book API endpoint with serializer, view , and URL

--- api/models.py
+++ api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length = 100)
-    author = models.CharField(max_length= 100)
-    
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(default=timezone.now)  # proper default
+
     def __str__(self):
         return self.title
